4e88379eb53c5c65949ddfd30f278bc973e5769e
georgehpj <hupeijiegeorge@hotmail.com>
2017-03-05 22:08:17 +0800

    ui

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ const gulp = require("gulp");
 const sass = require('gulp-sass');
 
 gulp.task('_buildScss', function () {
-    gulp.src('./vip-ui/src/stylesheets/vip-ui.scss')
+    gulp.src('./src/stylesheets/vip-ui.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./vip-ui/src/stylesheets/'));
+        .pipe(gulp.dest('./src/stylesheets/'));
 });
 
 //在命令行执行：gulp sass:watch，就可实现监听文件变化来自动编译
 gulp.task('scssWatcher', function () {
-    gulp.watch('./vip-ui/src/stylesheets/**/*.scss', ["_buildScss"]);
+    gulp.watch('./src/stylesheets/**/*.scss', ["_buildScss"]);
 });
